77e4f6eeb3c05d4c1227e2ae9cff2fa714bff51d
bochill-stac <kimetsukids828@gmail.com>
2026-09-21 19:51:43 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // ローカル設定。GitHubにはコミットしません。
 window.STAC_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_PUBLISHABLE_KEY: ""
+  SUPABASE_URL: "https://dhspuugudpchqvrmmiku.supabase.co/rest/v1/",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_NKUR43sjFNsanZbck2xXfA_od_ZFmxW"
 };
